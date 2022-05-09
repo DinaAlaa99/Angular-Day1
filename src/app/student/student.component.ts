19adbae4eb1b3062ad8cc51583a5648c2ad9e899
DinaAlaa99 <37483521+DinaAlaa99@users.noreply.github.com>
2022-05-09 13:28:24 +0200

    Day1

---
+++ src/app/student/student.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "xyz",
+    templateUrl: "./student.component.html",
+    styleUrls:["./student.component.css"]
+})
+export class StudentComponent {
+
+}
+
